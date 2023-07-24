42bca76bd95298c949fe8c4fcacb42b91f9938aa
ci-cada <ciphedc@gmail.com>
2023-07-24 14:51:34 +0300

    Day 19 Update

---
+++ Day_19/Example.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Example{
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Lemon");
+        queue.add("Mango");
+
+        System.out.println(queue);
+    }
+}
